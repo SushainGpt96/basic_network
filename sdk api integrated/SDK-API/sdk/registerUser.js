1940b91ef0302e6e5d4685d20981cc5c2da1196d
SushainGpt96 <sushain.gpt96@gmail.com>
2021-11-02 10:53:07 +0530

    db update

--- sdk api integrated/SDK-API/sdk/registerUser.js
+++ sdk api integrated/SDK-API/sdk/registerUser.js
@@ -23,10 +23,15 @@ module.exports.registerUser = async (username, org) => {
     const caURL = ccp.certificateAuthorities[`ca.${org}.product.com`].url;
     const ca = new FabricCAServices(caURL);
 
-    // Create a new file system based wallet for managing identities.
-    const walletPath = path.join(process.cwd(), `wallet-${org}`);
-    const wallet = await Wallets.newFileSystemWallet(walletPath);
-    console.log(`Wallet path: ${walletPath}`);
+    // // Create a new file system based wallet for managing identities.
+    // const walletPath = path.join(process.cwd(), `wallet-${org}`);
+    // const wallet = await Wallets.newFileSystemWallet(walletPath);
+    // console.log(`Wallet path: ${walletPath}`);
+
+    const wallet = await Wallets.newCouchDBWallet(
+      "https://admin:adminpw@localhost:11984",
+      `${org}_db`
+    );
 
     // Check to see if we've already enrolled the user.
     const userIdentity = await wallet.get(username);
